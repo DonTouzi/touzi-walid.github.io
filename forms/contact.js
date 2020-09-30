9e805cea6973d53ddc30bae709bd37f433b37b50
Don T <59922406+DonTouzi@users.noreply.github.com>
2020-09-30 14:53:30 +0200

    Update contact.js

--- forms/contact.js
+++ forms/contact.js
@@ -1,60 +1,42 @@
-    var form_id_js = "javascript_form";
+//update this with your $form selector
+    var form_id = "jquery_form";
 
-    var data_js = {
+    var data = {
         "access_token": "{your access token}" // sent after you sign up
     };
 
-    function js_onSuccess() {
+    function onSuccess() {
         // remove this to avoid redirect
         window.location = window.location.pathname + "?message=Email+Successfully+Sent%21&isError=0";
     }
 
-    function js_onError(error) {
+    function onError(error) {
         // remove this to avoid redirect
         window.location = window.location.pathname + "?message=Email+could+not+be+sent.&isError=1";
     }
 
-    var sendButton = document.getElementById("js_send");
+    var sendButton = $("#" + form_id + " [name='send']");
 
-    function js_send() {
-        sendButton.value='Sending…';
-        sendButton.disabled=true;
-        var request = new XMLHttpRequest();
-        request.onreadystatechange = function() {
-            if (request.readyState == 4 && request.status == 200) {
-                js_onSuccess();
-            } else
-            if(request.readyState == 4) {
-                js_onError(request.response);
-            }
-        };
+    function send() {
+        sendButton.val('Sending…');
+        sendButton.prop('disabled',true);
 
-        var subject = document.querySelector("#" + form_id_js + " [name='subject']").value;
-        var message = document.querySelector("#" + form_id_js + " [name='text']").value;
-        data_js['subject'] = subject;
-        data_js['text'] = message;
-        var params = toParams(data_js);
+        var subject = $("#" + form_id + " [name='subject']").val();
+        var message = $("#" + form_id + " [name='text']").val();
+        data['subject'] = subject;
+        data['text'] = message;
 
-        request.open("POST", "https://postmail.invotes.com/send", true);
-        request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-        request.send(params);
+        $.post('https://postmail.invotes.com/send',
+            data,
+            onSuccess
+        ).fail(onError);
 
         return false;
     }
 
-    sendButton.onclick = js_send;
+    sendButton.on('click', send);
 
-    function toParams(data_js) {
-        var form_data = [];
-        for ( var key in data_js ) {
-            form_data.push(encodeURIComponent(key) + "=" + encodeURIComponent(data_js[key]));
-        }
-
-        return form_data.join("&");
-    }
-
-    var js_form = document.getElementById(form_id_js);
-    js_form.addEventListener("submit", function (e) {
-        e.preventDefault();
+    var $form = $("#" + form_id);
+    $form.submit(function( event ) {
+        event.preventDefault();
     });
