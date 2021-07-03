80d7f9a20ae569676fcf213cac54d6ebaad3c87c
Don T <59922406+DonTouzi@users.noreply.github.com>
2021-07-04 01:38:54 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -107,22 +107,4 @@
     offset: '80%'
   });
 
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      768: {
-        items: 2
-      },
-      900: {
-        items: 3
-      }
-    }
-  });
-
 })(jQuery);
